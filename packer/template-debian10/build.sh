47b2b34c05fc74f15aeb72365beffd0cfd1cddee
kidrek <kidrek@gmail.com>
2020-08-10 16:02:26 +0200

    Update build.sh

--- packer/template-debian10/build.sh
+++ packer/template-debian10/build.sh
@@ -2,14 +2,19 @@
 
 
 ## Remove old files
-rm -rf ./ova/template-debian10.ova 2>/dev/null
+### Remove old ova
+NAME="`cat debian10.json| egrep '"name":' | awk -F ":" '{print $2}' | sed 's/"//g' | sed 's/,//g' | sed 's/ //g'`.ova"
+DIRECTORY=`cat debian10.json| egrep 'output_directory' | awk -F ":" '{print $2}' | sed 's/"//g' | sed 's/,//g'`
+rm -rf $DIRECTORY/$NAME
+## Remove old log file
+rm -f build.log
 
-## Generate machine
-rm build.log
+## Generate preseed with strong password
+password=`../SCRIPTS/generate-password.sh`
+sed "s/<password>/$password/" ./http/preseed.cfg.tpl > ./http/preseed.cfg
+
+## Generate new ova
 export PACKER_LOG=1; packer build debian10.json | tee -a build.log
-ret=${?}
-
-## Send logs and result in my slack channel
 if [ `tail -n 50 build.log | egrep "Failed to prepare build|Builds finished but no artifacts were created" | grep -v "grep" | wc -l` -eq 0 ]; then
   echo "Packer [template-debian10] : Success" | ../../slack-msg.sh
 else
@@ -19,6 +24,6 @@ fi
 
 ## Flush memory cache
 echo 3 | sudo tee /proc/sys/vm/drop_caches
-
-## Remove cache directory
+## Remove temp files
 rm -rf ./packer_cache
+rm -rf ./http/preseed.cfg
