ee77d10a69e1a6773ed5807d928003132ced13e9
kidrek <kidrek@gmail.com>
2020-08-11 12:53:51 +0200

    Update build.sh

--- packer/template-debian10/build.sh
+++ packer/template-debian10/build.sh
@@ -6,15 +6,17 @@
 NAME="`cat debian10.json| egrep '"name":' | awk -F ":" '{print $2}' | sed 's/"//g' | sed 's/,//g' | sed 's/ //g'`.ova"
 DIRECTORY=`cat debian10.json| egrep 'output_directory' | awk -F ":" '{print $2}' | sed 's/"//g' | sed 's/,//g'`
 rm -rf $DIRECTORY/$NAME
-## Remove old log file
+### Remove old log file
 rm -f build.log
-
-## Generate preseed with strong password
-password=`../SCRIPTS/generate-password.sh`
-sed "s/<password>/$password/" ./http/preseed.cfg.tpl > ./http/preseed.cfg
 
 ## Generate new ansible SSH keys
 yes yes | ssh-keygen -q -t rsa -N "" -f ../FILES/ansible.key -b 4096 -C "ansible@pin.local"
+
+## Generate preseed with strong password
+password_root=`../SCRIPTS/generate-password.sh`
+password_ansible=`../SCRIPTS/generate-password.sh`
+ansible_ssh_key=`cat ../FILES/ansible.key.pub`
+sed "s/<password_root>/$password_root/; s/<password_ansible>/$password_ansible/; s|<ansible_ssh_key>|$ansible_ssh_key|" ./http/preseed.cfg.tpl > ./http/preseed.cfg
 
 ## Generate new ova
 export PACKER_LOG=1; packer build debian10.json | tee -a build.log
