b80576782ea033470f3dc1a01fda7800eb368dcc
kidrek <kidrek@gmail.com>
2020-11-20 09:13:43 +0100

    Update 00_network.tf

--- terraform/00_network.tf
+++ terraform/00_network.tf
@@ -1,17 +1,17 @@
 
-resource "esxi_virtual_switch" "pin-vswitch" {
+resource "esxi_virtual_switch" "dfirlab-vswitch" {
   count             = 1
-  virtual_switch_name = "PIN-${count.index + 1}-vswitch"
+  virtual_switch_name = "DFIRLab-${count.index + 1}-vswitch"
 }
 
-resource "esxi_port_group" "pin-vm" {
+resource "esxi_port_group" "dfirlab-vm" {
   count             = 1
-  port_group_name   = "PIN-${count.index + 1}-vm"
-  virtual_switch_id = esxi_virtual_switch.pin-vswitch[count.index].id
+  port_group_name   = "DFIRLab-${count.index + 1}-vm"
+  virtual_switch_id = esxi_virtual_switch.dfirlab-vswitch[count.index].id
 }
 
-resource "esxi_port_group" "pin-malware-analysis" {
+resource "esxi_port_group" "dfirlab-malware-analysis" {
   count             = 1
-  port_group_name   = "PIN-${count.index + 1}-malware-analysis"
-  virtual_switch_id = esxi_virtual_switch.pin-vswitch[count.index].id
+  port_group_name   = "DFIRLab-${count.index + 1}-malware-analysis"
+  virtual_switch_id = esxi_virtual_switch.dfirlab-vswitch[count.index].id
 }
