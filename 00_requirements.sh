f935e8ab85356077fe430dffdee23178389a096d
kidrek <kidrek@gmail.com>
2020-12-03 15:35:13 +0100

    add script to download all required files

---
+++ 00_requirements.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Download virtio drivers
+cd packer/ISO--drivers
+wget https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
+cd -
+
+# Download Microsoft Windows10 iso
+cd packer/ISO
+wget https://software-download.microsoft.com/download/pr/19041.264.200511-0456.vb_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso
+mv 19041.264.200511-0456.vb_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso win10_19041.264.200511-0456.vb_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso 
+cd -
+
+# Download Debian iso
+cd packer/ISO
+wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-10.6.0-amd64-netinst.iso
+cd -
