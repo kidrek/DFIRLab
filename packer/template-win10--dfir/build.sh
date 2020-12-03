dd55e5dff7897a496f7a0761a35a7f497e6285ba
kidrek <kidrek@gmail.com>
2020-12-03 15:17:25 +0100

    add rearm windows script

--- packer/template-win10--dfir/build.sh
+++ packer/template-win10--dfir/build.sh
@@ -6,13 +6,9 @@
 NAME="`cat win10.json| egrep '"name":' | awk -F ":" '{print $2}' | sed 's/"//g' | sed 's/,//g' | sed 's/ //g'`.ova"
 DIRECTORY=`cat win10.json| egrep 'output_directory' | awk -F ":" '{print $2}' | sed 's/"//g' | sed 's/,//g'`
 rm -rf $DIRECTORY/$NAME
-## Remove old log file
-rm -f build.log
 
 ## Generate new ova
-date  | tee -a build.log; export PACKER_LOG=1; packer build -var-file=../variables.json  win10.json | tee -a build.log; date | tee -a build.log
+date  | tee -a build.log; export PACKER_LOG=1; packer build -var-file=../variables.json  win10.json | tee -a build.log; date | tee build.log
 
-## Flush memory cache
-echo 3 | sudo tee /proc/sys/vm/drop_caches
 ## Remove temp files
 rm -rf ./packer_cache
