3bdfe56f2824c10756cbcf73767a11c9783aee5c
kidrek <kidrek@gmail.com>
2020-12-03 10:30:00 +0100

    Solve some issues

--- packer/SCRIPTS/generate-password.sh
+++ packer/SCRIPTS/generate-password.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-64};echo;
+#< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-64};echo;
+openssl rand -base64 1000 | tr -cd "[:alnum:]" | tr -d "lo" | cut -c 1-64
