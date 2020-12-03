d8dfbf9f7809b132e59d1d19dd935d18ef9150ac
kidrek <kidrek@gmail.com>
2020-12-03 13:40:07 +0100

    add rearm windows script

--- packer/template-win10/build.sh
+++ packer/template-win10/build.sh
@@ -5,10 +5,7 @@
 rm -rf ./template-Win10.ova 2>/dev/null
 
 ## Generate machine
-export PACKER_LOG=1; packer build win10.json
-
-## Flush memory cache
-echo 3 | sudo tee /proc/sys/vm/drop_caches 1>/dev/null
+export PACKER_LOG=1; packer build -var-file=../variables.json win10.json | tee build.log
 
 ## Remove cache directory
 rm -rf ./packer_cache
